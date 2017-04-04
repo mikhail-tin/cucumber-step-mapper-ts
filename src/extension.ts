a87fd08673267666e014cfb8aed6bac3e4510195
Mikhail-Tuvykin <Mikhail.Tuvykin@returnonintelligence.com>
2017-04-04 17:38:22 +0300

    init

---
+++ src/extension.ts
@@ -0,0 +1,70 @@
+'use strict';
+import * as vscode from 'vscode';
+const fs = require('fs');
+const path = require('path');
+
+export function activate(context: vscode.ExtensionContext) {
+    let steps: IMember[] = [];
+    let listOfFiles = [];
+
+    let fillStore = () => {
+        steps = [];
+        listOfFiles = [];
+        let getAllFiles = function(myPath) {
+            if( fs.existsSync(myPath) ) {
+                fs.readdirSync(myPath).forEach(function(file,index) {
+                    let curPath = myPath + "/" + file;
+                    if(fs.lstatSync(curPath).isDirectory()) { 
+                        getAllFiles(curPath);
+                    } else { 
+                        if( path.extname(curPath) == '.ts'){
+                            listOfFiles.push(curPath);
+                        }
+                    }
+                });
+            }
+        };
+    
+        getAllFiles(path.join(vscode.workspace.rootPath, 'src'));
+
+        listOfFiles.forEach((file)=>{
+            let lines = fs.readFileSync(file).toString().split("\n");
+            lines.forEach((i, line)=>{
+                if (i.indexOf('(/^') >= 0) {
+                    steps.push({
+                        regex: i.substring(i.indexOf('(/^')+3, i.indexOf('$/,')),
+                        file: file,
+                        line: line
+                    });
+                }
+            })
+        });
+    }
+
+    fillStore();
+
+    let goToDef = vscode.commands.registerCommand('extension.goToDef', async () => {
+        let step = vscode.window.activeTextEditor.document.lineAt(vscode.window.activeTextEditor.selection.active.line).text.trim();
+        let result = steps.find((elem) => {return step.search(elem.regex)>= 0;});
+        let document = await vscode.workspace.openTextDocument(result.file);
+        let textEditor = await vscode.window.showTextDocument(document);
+        let position = new vscode.Position(result.line, 0);
+        let range = new vscode.Range(position, position);
+        await textEditor.revealRange(range);
+        vscode.window.setStatusBarMessage('Go to step definition: ' + step);
+    });
+
+    let updateStore = vscode.workspace.onDidSaveTextDocument((textDocument: vscode.TextDocument) => {
+        fillStore();
+    });
+
+    context.subscriptions.push(goToDef);
+
+    context.subscriptions.push(updateStore)
+}
+
+export interface IMember {
+    regex: string;
+    file: string;
+    line: number;
+}
---
+++ vsc-extension-quickstart.md
@@ -0,0 +1,4 @@
+# This is cucumber extension for fast find step definition (only TypeScript support).
+How use:
+1. Click on your step in feature file 
+2. run your command from the command palette by pressing (`Ctrl+Shift+P` or `Cmd+Shift+P` on Mac) and typing `Hello World`
