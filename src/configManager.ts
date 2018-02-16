fb6d618cdd19b957a5c0ce70bd8f2be98cc3023c
Mikhail-Tuvykin <Mikhail.Tuvykin@returnonintelligence.com>
2018-02-16 11:39:33 +0300

    minor

--- src/configManager.ts
+++ src/configManager.ts
@@ -27,8 +27,8 @@ export class ConfigManager {
     private getPathToSrc(): string[] {
         let value = String(vscode.workspace.getConfiguration('cucumber-step-mapper').get('srcPath'));
         let arr = value.split(',');
-        let arr2 = arr.map(x=> {return path.join(vscode.workspace.rootPath, x.trim()) });
-        let arr3 = arr2.map(x=> { if(!(x.endsWith("/") || x.endsWith("\\"))) return x += "/";});
+        let arr2 = arr.map(x=> {return path.normalize(path.join(vscode.workspace.rootPath, x.trim())) });
+        let arr3 = arr2.map(x=> { if(!(x.endsWith("/") || x.endsWith("\\"))) return x += path.sep;});
         return arr3;
     }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,8 @@
 import {ExtensionContext, commands, window, workspace, TextDocument} from 'vscode';
 import {TextEditor, Range, Position, TextEditorRevealType, Selection} from 'vscode';
 import {GetListOfFiles, GetStepDef, IStepDef} from './fs';
-import {ConfigManager} from './configManager'
+import {ConfigManager} from './configManager';
+const steps: string[] = ['Given','When','Then','And','But'];
 
 export function activate(context: ExtensionContext) {
     let steps: IStepDef[] = [];
@@ -26,15 +27,18 @@ export function activate(context: ExtensionContext) {
         await scrollToNewPositon(result);
     });
 
-    let updateStore = workspace.onDidSaveTextDocument((td: TextDocument) => (td.languageId == config.typeOfSrc) && fillStore() );
+    let updateStore = workspace.onDidSaveTextDocument((td: TextDocument) => {
+        if (td.languageId == config.typeOfSrc && config.pathToSrc.filter(x => td.fileName.indexOf(x) >= 0).length > 0) 
+            fillStore() 
+        });
 
     context.subscriptions.push(goToDef);
     context.subscriptions.push(updateStore)
 }
 
-let isStep = (step) => { return step.indexOf("en ") >= 0 || step.indexOf("And ") >= 0 || step.indexOf("But ") >= 0 };
-let extractStep = (step) => { return step.replace('Given', '').replace('When', '').replace('Then', '').replace('And', '').replace('But', '').trim();};
-let sendMsgToStatusBar = (msg) => {window.setStatusBarMessage(`Cucumber-mapper: ${msg}.`, 5000);};
+let isStep = step => step.indexOf("en ") >= 0 || step.indexOf("And ") >= 0 || step.indexOf("But ") >= 0;
+let extractStep = step => step.replace('Given', '').replace('When', '').replace('Then', '').replace('And', '').replace('But', '').trim();
+let sendMsgToStatusBar = msg => window.setStatusBarMessage(`Cucumber-mapper: ${msg}.`, 5000);
 let scrollToNewPositon = async (result: IStepDef): Promise<void> => {
     let document = await workspace.openTextDocument(result.file);
     let textEditor = await window.showTextDocument(document);
